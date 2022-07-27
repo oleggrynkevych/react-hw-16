41430f853971bc29ad9e4c769c19722481eea2ed
oleggrynkevych <oleg252.og@gmail.com>
2022-07-28 01:47:40 +0300

    Updates

---
+++ precache-manifest.13dcc8c6f40db885381957baaa11d8f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec54c3deebdeb2464936",
+    "url": "/react-hw-16/static/css/main.978557f0.chunk.css"
+  },
+  {
+    "revision": "ec54c3deebdeb2464936",
+    "url": "/react-hw-16/static/js/main.ec54c3de.chunk.js"
+  },
+  {
+    "revision": "a8fca786be699977fd58",
+    "url": "/react-hw-16/static/js/1.a8fca786.chunk.js"
+  },
+  {
+    "revision": "e7e12cad25848f5ea57a",
+    "url": "/react-hw-16/static/js/runtime~main.e7e12cad.js"
+  },
+  {
+    "revision": "fd980f526d481965052f73fa7373fe2b",
+    "url": "/react-hw-16/index.html"
+  }
+];
